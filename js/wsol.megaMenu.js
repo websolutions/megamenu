13aacf9a5a25996bf147c6ae13e47ba386cdc7a5
Nick Melville <nmelville@wsol.com>
2015-01-09 16:47:49 -0600

    Removed hoverIntent dependency, updated to 3.0

--- js/wsol.megaMenu.js
+++ js/wsol.megaMenu.js
@@ -21,16 +21,63 @@
 
       base.$items = base.$el.find(base.options.itemSelector);
 
-      // Handle events
-      base.$items.hoverIntent($.extend({}, base.options, {
-        over: base.overHandler,
-        out: base.outHandler
-      }));
-      base.$items.find(base.options.subMenuSelector).find(base.options.subMenuItemSelector)
-          .hoverIntent($.extend({}, base.options, {
-        over: function(event) { $(this).addClass(base.options.hoverClass); },
-        out: function(event) { $(this).removeClass(base.options.hoverClass); }
-      }));
+      // Handle interaction on top level items
+      base.$items
+        .on( 'mouseenter touchstart', base.overHandler )
+        .on( 'mouseleave', function( evt ) {
+          $(this).removeClass(base.options.hoverClass).blur();
+        } );
+
+      // Handle interaction on submenus
+      base.$items
+        .find( base.options.subMenuSelector )
+        .find( base.options.subMenuItemSelector )
+          .on('mouseenter', function ( evt ) {
+            evt.stopImmediatePropagation();
+            $(this).addClass(base.options.hoverClass);
+          } )
+          .on('mouseleave', function () {
+            $(this).removeClass( base.options.hoverClass ).blur();
+          })
+          .on('touchstart', function ( evt ) {
+            evt.stopImmediatePropagation();
+            if ( $(this).hasClass( base.options.hoverClass )) {
+              $(this).addClass( base.options.hoverClass );
+            } else {
+              $(this).removeClass( base.options.hoverClass ).blur();
+            }
+          } );
+
+      // Add something to click/tap to expand responsive sub-menus/panels
+      base.$items
+        .find( base.options.subMenuSelector )
+        .siblings('a')
+        .wrap('<span class="expandable"></span>')
+        .before('<span class="expand"><span class="plus">+</span><span class="minus">&ndash;</span></span>');
+
+      base.$items
+        .find( base.options.subSubMenuSelector )
+        .siblings('a')
+        .wrap('<span class="flyout-expandable"></span>')
+        .before('<span class="flyout-expand"><span class="plus">+</span><span class="minus">&ndash;</span></span>')
+        .parents( base.options.subMenuSelector ).addClass('has-flyout');
+
+      // Handle click/tap on newly added +/-
+      base.$items.find( ".expand" ).on( 'click' , function( evt ) {
+        evt.stopPropagation();
+        $(this).closest('li')
+          .toggleClass( base.options.hoverTouchClass )
+          .siblings()
+            .removeClass( base.options.hoverTouchClass );
+      } );
+
+      base.$items.find( ".flyout-expand" ).on( 'click' , function( evt ) {
+        evt.stopPropagation();
+        $(this).closest('li')
+          .toggleClass( base.options.hoverTouchClass )
+          .siblings()
+            .removeClass( base.options.hoverTouchClass );
+      } );
     };
 
     base.overHandler = function() {
@@ -85,10 +132,15 @@
       if (base.options.out) {
         base.options.out.call(event.target, event, _);
       }
-    }
+    };
 
     base.destroy = function() {
-      base.$items.removeClass(base.options.hoverClass);
+      console.log(base);
+      base.$items.removeClass( base.options.hoverClass ).removeClass( base.options.hoverTouchClass );
+      base.$items.find( '.expand' ).remove();
+      base.$items.find( '.expandable' ).children().unwrap();
+      base.$items.find( '.flyout-expand' ).remove();
+      base.$items.find( '.flyout-expandable' ).children().unwrap();
 
       // Loop through submenus
       base.$items.find(base.options.subMenuSelector).each(function () {
@@ -112,13 +164,6 @@
   };
 
   $.wsol.megamenu.defaultOptions = {
-    // HoverIntent config
-    sensitivity: 2,
-    interval: 10,
-    timeout: 50,
-    over: null,
-    out: null,
-
     // Custom config
     itemSelector: "> li",
     subMenuSelector: ".dropdown",
@@ -126,6 +171,7 @@
     subSubMenuSelector: ".dropdown-panel",
     subSubMenuColSelector: ".dropdown-panel-menu",
     hoverClass: "hover",
+    hoverTouchClass: "active",
     revClass: "rev"
   };
 
